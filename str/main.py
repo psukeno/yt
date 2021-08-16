98ae6e9ff274dda77ad14955edaa79b4410dad4b
psukeno <soujyu.ef.p@gmail.com>
2021-08-17 00:37:25 +0900

    2nd commit

--- str/main.py
+++ str/main.py
@@ -6,11 +6,11 @@ st.title('Streamlit 入門')
 
 st.write("DataFrame")
 
-left_column,right_column = st.beta_columns(2)
+left_column,right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
 if button:
     right_column.write('ててて')
-expander = st.beta_expander("問い合わせ内容")
+expander = st.expander("問い合わせ内容")
 expander.write("問い合わせ内容を書く")
 # df = pd.DataFrame({
 #     '1列目': [1,2,3,4],
